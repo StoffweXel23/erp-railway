d7daca8a9965587df3cfeec92e0297f809975463
StoffweXel23 <m-leicht@arcor.de>
2025-05-22 22:29:13 +0200

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,12 @@ log() {
 log "Umgebungsvariablen:"
 env | sort
 
+# Port-Validierung
+if [ -z "${PORT}" ]; then
+  log "WARNUNG: PORT nicht gesetzt, verwende Standardport 8000"
+  export PORT=8000
+fi
+
 # Site anlegen, falls nicht vorhanden
 if [ ! -d "/home/frappe/frappe-bench/sites/$SITE_NAME" ]; then
   log "Lege neue Site $SITE_NAME an..."
@@ -29,6 +35,10 @@ if [ ! -d "/home/frappe/frappe-bench/sites/$SITE_NAME" ]; then
   bench --site "$SITE_NAME" set-config redis_queue "${RAILWAY_REDIS_URL}"
   bench --site "$SITE_NAME" set-config redis_socketio "${RAILWAY_REDIS_URL}"
 
+  # Setze den Webserver-Port
+  log "Setze Webserver-Port auf ${PORT}..."
+  bench --site "$SITE_NAME" set-config webserver_port "${PORT}"
+
   # Build assets
   log "Baue Assets..."
   bench build
@@ -42,6 +52,10 @@ else
   bench --site "$SITE_NAME" set-config redis_cache "${RAILWAY_REDIS_URL}"
   bench --site "$SITE_NAME" set-config redis_queue "${RAILWAY_REDIS_URL}"
   bench --site "$SITE_NAME" set-config redis_socketio "${RAILWAY_REDIS_URL}"
+  
+  # Aktualisiere den Webserver-Port
+  log "Aktualisiere Webserver-Port auf ${PORT}..."
+  bench --site "$SITE_NAME" set-config webserver_port "${PORT}"
 fi
 
 # Debug: Zeige Site-Status
@@ -50,7 +64,7 @@ bench --site "$SITE_NAME" show-config
 
 # Production-Start
 if [ "$PRODUCTION" = "1" ]; then
-  log "Starte ERPNext mit gunicorn (Production-Modus)..."
+  log "Starte ERPNext mit gunicorn (Production-Modus) auf Port ${PORT}..."
   cd /home/frappe/frappe-bench
   exec /home/frappe/frappe-bench/env/bin/gunicorn -b 0.0.0.0:${PORT} frappe.app:application --log-level debug
 else
