f67af94d170fda72bf5ff47be4e8ea4d3ee2e977
StoffweXel23 <m-leicht@arcor.de>
2025-05-22 22:16:51 +0200

    Delete update-config.sh

--- update-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat > /home/frappe/frappe-bench/sites/localhost/site_config.json << EOF
-{
-    "db_host": "mariadb",
-    "db_name": "${DB_NAME}",
-    "db_password": "${DB_PASSWORD}",
-    "db_type": "mariadb"
-}
-EOF 
